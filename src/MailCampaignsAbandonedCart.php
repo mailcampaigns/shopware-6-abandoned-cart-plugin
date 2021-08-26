618ceed2af6ceb3bbfda1f707a56eb543062bbc2
Twan Haverkamp <twan@mailcampaigns.nl>
2021-08-26 15:07:37 +0200

    Initial commit

---
+++ src/MailCampaignsAbandonedCart.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace MailCampaigns\AbandonedCart;
+
+use Shopware\Core\Framework\Plugin;
+
+/**
+ * @author Twan Haverkamp <twan@mailcampaigns.nl>
+ */
+class MailCampaignsAbandonedCart extends Plugin
+{
+}
