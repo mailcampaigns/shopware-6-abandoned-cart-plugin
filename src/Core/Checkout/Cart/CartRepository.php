45356cdffabfd5d9f06e3ab9848feddd3b9220f4
Jelle <100683754+JelleMailcampaigns@users.noreply.github.com>
2023-03-01 13:45:23 +0100

    Update CartRepository.php

--- src/Core/Checkout/Cart/CartRepository.php
+++ src/Core/Checkout/Cart/CartRepository.php
@@ -50,7 +50,7 @@ class CartRepository
 
             WHERE abandoned_cart.id IS NULL
             AND cart.`name` IN ($selectAbandonedCartNamesQuery)
-            AND cart.`customer_id` != null
+            AND cart.`customer_id` IS NOT NULL
             AND cart.`customer_id` != ''
 
             ORDER BY cart.created_at
