5b22498fdc6a3f26ddbba72dba7cb36d4d6b0a20
Max Seelig <maxseelig@gmail.com>
2025-03-24 13:22:22 +0100

    Implement uninstall method to handle data removal for abandoned cart plugin

--- src/MailCampaignsAbandonedCart.php
+++ src/MailCampaignsAbandonedCart.php
@@ -4,11 +4,28 @@ declare(strict_types=1);
 
 namespace MailCampaigns\AbandonedCart;
 
+use MailCampaigns\AbandonedCart\Migration\Migration1725548117CreateAbandonedCartTable;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Doctrine\DBAL\Connection;
 
 /**
  * @author Twan Haverkamp <twan@mailcampaigns.nl>
  */
 class MailCampaignsAbandonedCart extends Plugin
 {
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        parent::uninstall($uninstallContext);
+
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        $container = $this->container;
+        $connection = $container->get(Connection::class);
+
+        $migration = new Migration1725548117CreateAbandonedCartTable();
+        $migration->updateDestructive($connection);
+    }
 }
