d953e50a4b8e8ab98d2773cc0ed636c34d4f9c91
Ruslan Belziuk <ruslan@belziuk.com>
2025-04-26 09:49:08 +0300

    Add protection against randomly missing tokens as getting NULL is theoretically possible

--- src/Core/Checkout/AbandonedCart/AbandonedCartManager.php
+++ src/Core/Checkout/AbandonedCart/AbandonedCartManager.php
@@ -73,6 +73,9 @@ final class AbandonedCartManager
 
             // Get the abandoned cart ID by token.
             $abandonedCartId = $this->findAbandonedCartIdByToken($abandonedCart->getCartToken());
+            if ($abandonedCartId === null) {
+                continue;
+            }
 
             $abandonedCart->setId($abandonedCartId);
 
